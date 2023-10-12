7981abcecfb16f863787c1bf9cd8eea362902886
saki <16546851+sakiko-2@users.noreply.github.com>
2023-10-12 19:33:54 +1300

    Assembly-line exercise

--- assembly-line/assembly_line.rb
+++ assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
 class AssemblyLine
+  PRODUCTION_NUM_PER_HOUR = 221
+
   def initialize(speed)
     @speed = speed
   end
 
+  def success_rate
+    if @speed >= 1 && @speed <= 4
+      1.0
+    elsif @speed >= 5 && @speed <= 8
+      0.9
+    elsif @speed == 9
+      0.8
+    else
+      0.77
+    end
+  end
+
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    PRODUCTION_NUM_PER_HOUR * @speed * success_rate
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
