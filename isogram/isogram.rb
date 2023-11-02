43b8459ea874117575819c1ceb9f9d381c158bec
saki <16546851+sakiko-2@users.noreply.github.com>
2023-11-03 00:19:19 +1300

    Isogram exercise

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,7 +1,6 @@
-=begin
-Write your code for the 'Isogram' exercise in this file. Make the tests in
-`isogram_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/isogram` directory.
-=end
+class Isogram
+  def self.isogram?(word)
+    word_array = word.gsub(/[^A-Z]/i, '').downcase.split('')
+    word_array.uniq == word_array
+  end
+end
