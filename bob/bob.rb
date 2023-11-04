3778838394f7bbfc3553144c6c2938cfb3a65ff1
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-04 22:57:35 +1300

    Refactor

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,19 +1,17 @@
-class Bob
+module Bob
   def self.hey(input)
     message = input.strip
     is_question = message.end_with?('?')
     is_yelling = message.match(/[[:upper:]]+/) && !message.match(/[[:lower:]]+/)
 
-    if message.empty?
-      'Fine. Be that way!'
-    elsif is_question && is_yelling
-      "Calm down, I know what I'm doing!"
-    elsif is_yelling
-      'Whoa, chill out!'
-    elsif is_question
-      'Sure.'
-    else
-      'Whatever.'
+    return 'Fine. Be that way!' if message.empty?
+    
+    if is_yelling
+      return is_question ? "Calm down, I know what I'm doing!" : 'Whoa, chill out!'
     end
+    
+    return 'Sure.' if is_question
+    
+    'Whatever.'
   end
 end
