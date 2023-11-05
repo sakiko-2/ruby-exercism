ad2b610c80573c10df8f98cc377dc26c60f37142
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-05 16:54:47 +1300

    Savings-account exercise

--- savings-account/savings_account.rb
+++ savings-account/savings_account.rb
@@ -1,13 +1,21 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    return 3.213 if balance < 0
+    return 0.5 if balance < 1000
+    return 1.621 if balance < 5000
+    return 2.475 if balance >= 5000
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance * (1 + interest_rate(balance) / 100)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    year = 0
+    while current_balance < desired_balance
+      current_balance = annual_balance_update(current_balance)
+      year += 1
+    end
+    year
   end
 end
