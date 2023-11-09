838f5b324768720271cd6d8709faf5403888296a
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-09 18:49:09 +1300

    Word-count exercise

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,7 +1,9 @@
-=begin
-Write your code for the 'Word Count' exercise in this file. Make the tests in
-`word_count_test.rb` pass.
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase.downcase.scan(/\b[\w']+\b/)
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/word-count` directory.
-=end
+  def word_count
+    @phrase.tally
+  end
+end
--- word-count/word_count_test.rb
+++ word-count/word_count_test.rb
@@ -10,91 +10,91 @@ class WordCountTest < Minitest::Test
   end
 
   def test_count_one_of_each_word
-    skip
+     # skip
     phrase = Phrase.new("one of each")
     counts = { "one" => 1, "of" => 1, "each" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_multiple_occurrences_of_a_word
-    skip
+    #  skip
     phrase = Phrase.new("one fish two fish red fish blue fish")
     counts = { "one" => 1, "fish" => 4, "two" => 1, "red" => 1, "blue" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_handles_cramped_lists
-    skip
+    #  skip
     phrase = Phrase.new("one,two,three")
     counts = { "one" => 1, "two" => 1, "three" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_handles_expanded_lists
-    skip
+     # skip
     phrase = Phrase.new("one,\ntwo,\nthree")
     counts = { "one" => 1, "two" => 1, "three" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_ignore_punctuation
-    skip
+     # skip
     phrase = Phrase.new("car: carpet as java: javascript!!&@$%^&")
     counts = { "car" => 1, "carpet" => 1, "as" => 1, "java" => 1, "javascript" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_include_numbers
-    skip
+     # skip
     phrase = Phrase.new("testing, 1, 2 testing")
     counts = { "testing" => 2, "1" => 1, "2" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_normalize_case
-    skip
+     # skip
     phrase = Phrase.new("go Go GO Stop stop")
     counts = { "go" => 3, "stop" => 2 }
     assert_equal counts, phrase.word_count
   end
 
   def test_with_apostrophes
-    skip
+     # skip
     phrase = Phrase.new("'First: don't laugh. Then: don't cry. You're getting it.'")
     counts = { "first" => 1, "don't" => 2, "laugh" => 1, "then" => 1, "cry" => 1, "you're" => 1, "getting" => 1, "it" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_with_quotations
-    skip
+     # skip
     phrase = Phrase.new("Joe can't tell between 'large' and large.")
     counts = { "joe" => 1, "can't" => 1, "tell" => 1, "between" => 1, "large" => 2, "and" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_substrings_from_the_beginning
-    skip
+     # skip
     phrase = Phrase.new("Joe can't tell between app, apple and a.")
     counts = { "joe" => 1, "can't" => 1, "tell" => 1, "between" => 1, "app" => 1, "apple" => 1, "and" => 1, "a" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_multiple_spaces_not_detected_as_a_word
-    skip
+     # skip
     phrase = Phrase.new(" multiple   whitespaces")
     counts = { "multiple" => 1, "whitespaces" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_alternating_word_separators_not_detected_as_a_word
-    skip
+     # skip
     phrase = Phrase.new(",\n,one,\n ,two \n 'three'")
     counts = { "one" => 1, "two" => 1, "three" => 1 }
     assert_equal counts, phrase.word_count
   end
 
   def test_quotation_for_word_with_apostrophe
-    skip
+     # skip
     phrase = Phrase.new("can, can't, 'can't'")
     counts = { "can" => 1, "can't" => 2 }
     assert_equal counts, phrase.word_count
