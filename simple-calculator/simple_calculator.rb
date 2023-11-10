a662176b0f13aea9c6d6c5f58369f177f1d15c1b
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-10 16:57:58 +1300

    Simple-calculator exercise

--- simple-calculator/simple_calculator.rb
+++ simple-calculator/simple_calculator.rb
@@ -1,7 +1,19 @@
-class SimpleCalculator
+module SimpleCalculator
+  class UnsupportedOperation < ::StandardError
+  end
+
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    raise UnsupportedOperation unless ALLOWED_OPERATIONS.include?(operation)
+    raise ArgumentError if !first_operand.is_a?(Integer) || !second_operand.is_a?(Integer)
+    
+    begin
+      result = first_operand.method(operation).(second_operand)
+      "#{first_operand} #{operation} #{second_operand} = #{result}"
+    rescue
+      ZeroDivisionError => e
+      'Division by zero is not allowed.'
+    end
   end
 end
