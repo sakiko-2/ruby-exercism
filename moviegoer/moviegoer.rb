56d7f145599550bff806ca38f036c3f9a83fee6a
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-11 18:22:45 +1300

    Moviegoer exercise

--- moviegoer/moviegoer.rb
+++ moviegoer/moviegoer.rb
@@ -1,4 +1,3 @@
-# This is a custom exception that you can use in your code
 class NotMovieClubMemberError < RuntimeError
 end
 
@@ -9,15 +8,14 @@ class Moviegoer
   end
 
   def ticket_price
-    raise 'Please implement the Moviegoer#ticket_price method'
+    @age >= 60 ? 10 : 15
   end
 
   def watch_scary_movie?
-    raise 'Please implement the Moviegoer#watch_scary_movie method'
+    @age >= 18
   end
 
-  # Popcorn is 🍿
   def claim_free_popcorn!
-    raise 'Please implement the Moviegoer#claim_free_popcorn method'
+    @member ? '🍿' : (raise NotMovieClubMemberError)
   end
 end
