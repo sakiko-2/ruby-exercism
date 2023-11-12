6b344767e7e1a693e0d6dd53067561fb107234c2
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-12 21:21:40 +1300

    List-ops exercise

--- list-ops/list_ops.rb
+++ list-ops/list_ops.rb
@@ -1,7 +1,54 @@
-=begin
-Write your code for the 'List Ops' exercise in this file. Make the tests in
-`list_ops_test.rb` pass.
+module ListOps
+  def self.arrays(arrays)
+    count = 0
+    arrays.each { count += 1 }
+    count
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/list-ops` directory.
-=end
+  def self.reverser(array)
+    length = arrays(array)
+    copy = array
+    i = 0
+    j = length - 1
+
+    while i < j
+      temp = copy[i]
+      array[i] = array[j]
+      array[j] = temp
+      i += 1
+      j -= 1
+    end
+
+    array
+  end
+
+  def self.concatter(*arrays)
+    result = []
+    arrays.each { |a| a.each { |el| result << el } }
+    result
+  end
+
+  def self.mapper(array)
+    result = []
+    array.each { |n| result << yield(n)}
+    result
+  end
+
+  def self.filterer(array, &block)
+    result = []
+    array.each { |n| result << n if block.call(n) }
+    result
+  end
+
+  def self.sum_reducer(array)
+    result = 0
+    array.each { |n| result += n }
+    result
+  end
+
+  def self.factorial_reducer(array)
+    result = 1
+    array.each { |n| result *= n }
+    result
+  end
+end
