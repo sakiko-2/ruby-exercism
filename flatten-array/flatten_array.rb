7a54379ac94f3f3d2ed765e2272355171418f925
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-12 22:57:48 +1300

    Flatten-array exercise

--- flatten-array/flatten_array.rb
+++ flatten-array/flatten_array.rb
@@ -1,7 +1,5 @@
-=begin
-Write your code for the 'Flatten Array' exercise in this file. Make the tests in
-`flatten_array_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/flatten-array` directory.
-=end
+module FlattenArray
+  def self.flatten(arr)
+    arr.flatten.compact
+  end
+end
--- flatten-array/flatten_array_test.rb
+++ flatten-array/flatten_array_test.rb
@@ -8,42 +8,42 @@ class FlattenArrayTest < Minitest::Test
   end
 
   def test_no_nesting
-    skip
+    # skip
     flat_array = FlattenArray.flatten([0, 1, 2])
     assert_equal [0, 1, 2], flat_array
   end
 
   def test_flattens_a_nested_array
-    skip
+    # skip
     assert_empty FlattenArray.flatten([[[]]])
   end
 
   def test_flattens_array_with_just_integers_present
-    skip
+    # skip
     flat_array = FlattenArray.flatten([1, [2, 3, 4, 5, 6, 7], 8])
     assert_equal [1, 2, 3, 4, 5, 6, 7, 8], flat_array
   end
 
   def test_5_level_nesting
-    skip
+    # skip
     flat_array = FlattenArray.flatten([0, 2, [[2, 3], 8, 100, 4, [[[50]]]], -2])
     assert_equal [0, 2, 2, 3, 8, 100, 4, 50, -2], flat_array
   end
 
   def test_6_level_nesting
-    skip
+    # skip
     flat_array = FlattenArray.flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8])
     assert_equal [1, 2, 3, 4, 5, 6, 7, 8], flat_array
   end
 
   def test_6_level_nest_list_with_null_values
-    skip
+    # skip
     flat_array = FlattenArray.flatten([0, 2, [[2, 3], 8, [[100]], nil, [[nil]]], -2])
     assert_equal [0, 2, 2, 3, 8, 100, -2], flat_array
   end
 
   def test_all_values_in_nested_list_are_null
-    skip
+    # skip
     assert_empty FlattenArray.flatten([nil, [[[nil]]], nil, nil, [[nil, nil], nil], nil])
   end
 end
