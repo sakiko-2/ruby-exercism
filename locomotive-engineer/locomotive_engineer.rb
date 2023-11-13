ca2f21147b46c730f37329d10e3e6791d7501dd3
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-13 22:25:40 +1300

    Locomotive-engineer exercise

--- locomotive-engineer/locomotive_engineer.rb
+++ locomotive-engineer/locomotive_engineer.rb
@@ -1,17 +1,18 @@
 class LocomotiveEngineer
-  def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+  def self.generate_list_of_wagons(*wagons)
+    wagons
   end
 
   def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
-    raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
+    a, b, c, *rest = each_wagons_id
+    [ c, *missing_wagons, *rest, a, b ]
   end
 
-  def self.add_missing_stops
-    raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
+  def self.add_missing_stops(route, **stops)
+    { **route, stops: stops.values }
   end
 
   def self.extend_route_information(route, more_route_information)
-    raise 'Please implement the LocomotiveEngineer.extend_route_information method'
+    { **route, **more_route_information }
   end
 end
