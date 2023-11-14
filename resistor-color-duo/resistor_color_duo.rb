3e99b210322d5785260833c50d4d6047252343e3
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-15 12:01:46 +1300

    Resistor-color-duo exercise

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,9 @@
-=begin
-Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
-`resistor_color_duo_test.rb` pass.
+module ResistorColorDuo
+  COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color-duo` directory.
-=end
+  def self.value(numbers)
+    a, b, *rest = numbers
+
+    COLORS.index(a) * 10 + COLORS.index(b)
+  end
+end
--- resistor-color-duo/resistor_color_duo_test.rb
+++ resistor-color-duo/resistor_color_duo_test.rb
@@ -8,32 +8,32 @@ class ResistorColorDuoTest < Minitest::Test
   end
 
   def test_blue_and_grey
-    skip
+    # skip
     assert_equal 68, ResistorColorDuo.value(%w[blue grey])
   end
 
   def test_yellow_and_violet
-    skip
+    # skip
     assert_equal 47, ResistorColorDuo.value(%w[yellow violet])
   end
 
   def test_white_and_red
-    skip
+    # skip
     assert_equal 92, ResistorColorDuo.value(%w[white red])
   end
 
   def test_orange_and_orange
-    skip
+    # skip
     assert_equal 33, ResistorColorDuo.value(%w[orange orange])
   end
 
   def test_ignore_additional_colors
-    skip
+    # skip
     assert_equal 51, ResistorColorDuo.value(%w[green brown orange])
   end
 
   def test_black_and_brown_one_digit
-    skip
+    # skip
     assert_equal 1, ResistorColorDuo.value(%w[black brown])
   end
 end
