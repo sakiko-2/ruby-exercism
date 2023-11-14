9b394eeb60c496fbe0c02b549fa30f58e40c350a
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-15 00:04:19 +1300

    Resistor-color exercise

--- resistor-color/resistor_color.rb
+++ resistor-color/resistor_color.rb
@@ -1,7 +1,7 @@
-=begin
-Write your code for the 'Resistor Color' exercise in this file. Make the tests in
-`resistor_color_test.rb` pass.
+module ResistorColor
+  COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue','violet', 'grey', 'white']
 
-To get started with TDD, see the `README.md` file in your
-`ruby/resistor-color` directory.
-=end
+  def self.color_code(color)
+    COLORS.find_index(color)
+  end
+end
