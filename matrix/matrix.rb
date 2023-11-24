586c61d6522ff1a71af3804b2fc384dc7d8e862e
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-24 18:18:25 +1300

    Matrix exercise

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,7 +1,15 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+class Matrix
+  attr_reader :matrix
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+  def initialize(matrix)
+    @matrix = matrix.split("\n")
+  end
+
+  def row(number)
+    matrix[number - 1].split(" ").map { |n| n.to_i }
+  end
+
+  def column(number)
+    matrix.map { |row| row.split(" ")[number - 1].to_i }
+  end
+end
