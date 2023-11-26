f00b2e74337d238198153d7bd33bc32b88d2a6cd
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2023-11-26 15:21:55 +1300

    Protein-translation exercise

--- protein-translation/protein_translation.rb
+++ protein-translation/protein_translation.rb
@@ -1,7 +1,23 @@
-=begin
-Write your code for the 'Protein Translation' exercise in this file. Make the tests in
-`protein_translation_test.rb` pass.
+class InvalidCodonError < StandardError
+end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/protein-translation` directory.
-=end
+module Translation
+  PROTEINS = { AUG: 'Methionine', UUU: 'Phenylalanine', UUC: 'Phenylalanine', 
+    UUA: 'Leucine', UUG: 'Leucine', UCU: 'Serine', UCC: 'Serine', UCA: 'Serine',
+    UCG: 'Serine', UAU: 'Tyrosine', UAC: 'Tyrosine', UGU: 'Cysteine',
+    UGC: 'Cysteine', UGG: 'Tryptophan', UAA: 'STOP', UAG: 'STOP', UGA: 'STOP'
+  }
+
+  def self.of_rna(strand)
+    codons = []
+    strand.chars.each_slice(3) {|codon| codons << codon.join }
+
+    proteins = []
+    codons.each do |codon|
+      raise InvalidCodonError unless PROTEINS[codon.to_sym]
+      PROTEINS[codon.to_sym] == 'STOP' ? break : proteins << PROTEINS[codon.to_sym]
+    end
+
+    proteins
+  end
+end
