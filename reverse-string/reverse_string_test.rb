e2d243fe015106e73241726d501927af8cc24125
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-01-29 01:42:02 +1300

    Reverse-string exercise

--- reverse-string/reverse_string_test.rb
+++ reverse-string/reverse_string_test.rb
@@ -8,27 +8,27 @@ class ReverserTest < Minitest::Test
   end
 
   def test_a_word
-    skip
+    # skip
     assert_equal "tobor", Reverser.reverse("robot")
   end
 
   def test_a_capitalized_word
-    skip
+    # skip
     assert_equal "nemaR", Reverser.reverse("Ramen")
   end
 
   def test_a_sentence_with_punctuation
-    skip
+    # skip
     assert_equal "!yrgnuh m'I", Reverser.reverse("I'm hungry!")
   end
 
   def test_a_palindrome
-    skip
+    # skip
     assert_equal "racecar", Reverser.reverse("racecar")
   end
 
   def test_an_even_sized_word
-    skip
+    # skip
     assert_equal "reward", Reverser.reverse("drawer")
   end
 end
