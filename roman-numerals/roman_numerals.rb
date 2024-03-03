ac93aa5cf4680b0a71effe2bd952737ca68d8f2d
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-03-03 16:24:28 +1300

    roman numerals

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -1,7 +1,17 @@
-=begin
-Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
-`roman_numerals_test.rb` pass.
+class Integer
+  ROMANS = {1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I'}
 
-To get started with TDD, see the `README.md` file in your
-`ruby/roman-numerals` directory.
-=end
+  def to_roman
+    number = self
+    result = ''
+
+    ROMANS.each {|k, v|
+      while number >= k
+        result << v
+        number -= k
+      end
+    }
+
+    return result
+  end
+end
--- roman-numerals/roman_numerals_test.rb
+++ roman-numerals/roman_numerals_test.rb
@@ -8,127 +8,127 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_2_is_ii
-    skip
+    # skip
     assert_equal "II", 2.to_roman
   end
 
   def test_3_is_iii
-    skip
+    # skip
     assert_equal "III", 3.to_roman
   end
 
   def test_4_is_iv
-    skip
+    # skip
     assert_equal "IV", 4.to_roman
   end
 
   def test_5_is_v
-    skip
+    # skip
     assert_equal "V", 5.to_roman
   end
 
   def test_6_is_vi
-    skip
+    # skip
     assert_equal "VI", 6.to_roman
   end
 
   def test_9_is_ix
-    skip
+    # skip
     assert_equal "IX", 9.to_roman
   end
 
   def test_27_is_xxvii
-    skip
+    # skip
     assert_equal "XXVII", 27.to_roman
   end
 
   def test_48_is_xlviii
-    skip
+    # skip
     assert_equal "XLVIII", 48.to_roman
   end
 
   def test_49_is_xlix
-    skip
+    # skip
     assert_equal "XLIX", 49.to_roman
   end
 
   def test_59_is_lix
-    skip
+    # skip
     assert_equal "LIX", 59.to_roman
   end
 
   def test_93_is_xciii
-    skip
+    # skip
     assert_equal "XCIII", 93.to_roman
   end
 
   def test_141_is_cxli
-    skip
+    # skip
     assert_equal "CXLI", 141.to_roman
   end
 
   def test_163_is_clxiii
-    skip
+    # skip
     assert_equal "CLXIII", 163.to_roman
   end
 
   def test_402_is_cdii
-    skip
+    # skip
     assert_equal "CDII", 402.to_roman
   end
 
   def test_575_is_dlxxv
-    skip
+    # skip
     assert_equal "DLXXV", 575.to_roman
   end
 
   def test_911_is_cmxi
-    skip
+    # skip
     assert_equal "CMXI", 911.to_roman
   end
 
   def test_1024_is_mxxiv
-    skip
+    # skip
     assert_equal "MXXIV", 1024.to_roman
   end
 
   def test_3000_is_mmm
-    skip
+    # skip
     assert_equal "MMM", 3000.to_roman
   end
 
   def test_16_is_xvi
-    skip
+    # skip
     assert_equal "XVI", 16.to_roman
   end
 
   def test_66_is_lxvi
-    skip
+    # skip
     assert_equal "LXVI", 66.to_roman
   end
 
   def test_166_is_clxvi
-    skip
+    # skip
     assert_equal "CLXVI", 166.to_roman
   end
 
   def test_666_is_dclxvi
-    skip
+    # skip
     assert_equal "DCLXVI", 666.to_roman
   end
 
   def test_1666_is_mdclxvi
-    skip
+    # skip
     assert_equal "MDCLXVI", 1666.to_roman
   end
 
   def test_3001_is_mmmi
-    skip
+    # skip
     assert_equal "MMMI", 3001.to_roman
   end
 
   def test_3999_is_mmmcmxcix
-    skip
+    # skip
     assert_equal "MMMCMXCIX", 3999.to_roman
   end
 end
