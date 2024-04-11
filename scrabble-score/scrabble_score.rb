d6f40602637165e085c3eeb04b36f12776924b85
sakiko-2 <16546851+sakiko-2@users.noreply.github.com>
2024-04-11 16:21:18 +1200

    scrabble-score

--- scrabble-score/scrabble_score.rb
+++ scrabble-score/scrabble_score.rb
@@ -1,7 +1,13 @@
-=begin
-Write your code for the 'Scrabble Score' exercise in this file. Make the tests in
-`scrabble_score_test.rb` pass.
+class Scrabble
+  attr_reader :word
 
-To get started with TDD, see the `README.md` file in your
-`ruby/scrabble-score` directory.
-=end
+  VALUES = { A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1, D: 2, G: 2, B: 3, C:3, M: 3, P: 3, F:4, H:4, V:4, W:4, Y:4, K: 5, J: 8, X: 8, Q: 10, Z: 10 }
+
+  def initialize(word)
+    @word = word
+  end
+
+  def score
+    word.upcase.chars.map { |c| VALUES[c.to_sym] }.sum
+  end
+end
